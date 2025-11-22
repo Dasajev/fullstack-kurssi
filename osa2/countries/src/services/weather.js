2e68fadc73cad3beba0f3f89d0e530d0f1ba3520
Daniel Liljeqvist <daniel.liljeqvist@iki.fi>
2025-11-22 10:27:53 +0200

    Updated to vite

--- osa2/countries/src/services/weather.js
+++ osa2/countries/src/services/weather.js
@@ -2,7 +2,7 @@ import axios from "axios";
 const baseUrl = "https://api.openweathermap.org/data/2.5/weather?q=";
 
 const getAll = async (city) => {
-  const apiKey = process.env.REACT_APP_API_KEY;
+  const apiKey = import.meta.env.VITE_API_KEY;
   const url = baseUrl + city + "&appid=" + apiKey + "&units=metric";
   const response = await axios.get(url);
   return response.data;
